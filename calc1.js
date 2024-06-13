aefe724b95c876bf3398135658a51cb77c32d543
Chimereucheya <150673013+Chimereucheya@users.noreply.github.com>
2024-06-13 22:20:25 +0100

    Add files via upload

---
+++ calc1.js
@@ -0,0 +1,23 @@
+
+let equations;
+let result= document.getElementById('field');
+let 
+function add() {
+let add =   " + " ; 
+  result.value += add
+}
+function subtract() {
+  let   subtract =   " - " ; 
+  result.value += subtract
+}
+function divide() {
+  let  divide =   " / " ; 
+  result.value += divide
+}
+function multiply() {
+  let  multiply =   " * " ; 
+  result.value += multiply
+}
+function equals() {
+result.value=eval(result.value)
+}
